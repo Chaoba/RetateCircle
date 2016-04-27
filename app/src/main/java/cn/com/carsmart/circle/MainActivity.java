a7bead1732e7eca6b7af0b7f33f1b6b1b1780ba4
mushuichuan <qqliys@126.com>
2016-04-27 10:47:13 +0800

    add some rudders

--- app/src/main/java/cn/com/carsmart/circle/MainActivity.java
+++ app/src/main/java/cn/com/carsmart/circle/MainActivity.java
@@ -2,6 +2,7 @@ package cn.com.carsmart.circle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
@@ -17,21 +18,16 @@ public class MainActivity extends Activity {
     ArrayList<Integer> contents = new ArrayList<>();
 
     LinearInterpolator lin = new LinearInterpolator();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        contents.add(R.mipmap.circle1);
-        contents.add(R.mipmap.circle2);
-        contents.add(R.mipmap.circle3);
-        contents.add(R.mipmap.circle4);
-        contents.add(R.mipmap.circle5);
-        contents.add(R.mipmap.circle6);
-        contents.add(R.mipmap.circle7);
-        contents.add(R.mipmap.circle8);
-        contents.add(R.mipmap.circle9);
-        contents.add(R.mipmap.circle10);
+        for (int i = 18; i > 0; i--) {
+            int id = getResources().getIdentifier("circle" + i, "mipmap", getPackageName());
+            contents.add(id);
+        }
         ListView list = (ListView) findViewById(R.id.list);
         list.setAdapter(new CircleAdapter());
     }
@@ -55,18 +51,38 @@ public class MainActivity extends Activity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View v = View.inflate(MainActivity.this, R.layout.item, null);
-            final ImageView imageView = (ImageView) v.findViewById(R.id.img);
-            imageView.setImageResource(contents.get(position));
-            final Animation operatingAnim  = AnimationUtils.loadAnimation(MainActivity.this, R.anim.circle);
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = View.inflate(MainActivity.this, R.layout.item, null);
+
+                holder = new ViewHolder();
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.img = (ImageView) convertView.findViewById(R.id.img);
+            Animation animation = holder.img.getAnimation();
+            if (animation != null) {
+                animation.cancel();
+            }
+            holder.img.setImageResource(contents.get(position));
+            Log.i("test", "id:" + holder.img.getId() + "   hashcod:" + holder.img.hashCode());
+            final Animation operatingAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.circle);
             operatingAnim.setInterpolator(lin);
-            v.setOnClickListener(new View.OnClickListener() {
+            final ViewHolder finalHolder = holder;
+            convertView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                        imageView.startAnimation(operatingAnim);
+                    finalHolder.img.startAnimation(operatingAnim);
                 }
             });
-            return v;
+            return convertView;
         }
     }
+
+    class ViewHolder {
+        ImageView img;
+    }
+
 }
